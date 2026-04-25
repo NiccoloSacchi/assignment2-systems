ffb477836a596e9bc1d3d3d79ea6a11da8e9f3ed
Niccolo Sacchi <niccsacchi@gmail.com>
2026-04-25 15:09:01 +0200

    Add instruction for Modal image with local LLM library.

--- cs336_systems/modal_setup.py
+++ cs336_systems/modal_setup.py
@@ -17,6 +17,37 @@ app_image = (
       "git clone https://github.com/NiccoloSacchi/assignment1-basics /root/assignment1-basics",
       "pip install -e /root/assignment1-basics",
     )
+
+    # -------------------------------------------------------------------
+    # If you have assignment1-basics repo locally and are applying changes to
+    # it, e.g. adding torch.profiler.record_function in the code, then you want
+    # to copy the local code to the Modal image instead of cloning from GitHub.
+    # This requires re-building the image so it is slower.
+    # .add_local_dir(
+    #   local_path="../assignment1-basics/cs336_basics",
+    #   remote_path="/root/assignment1-basics/cs336_basics",
+    #   copy=True,
+    # )
+    # .add_local_file(
+    #   local_path="../assignment1-basics/pyproject.toml",
+    #   remote_path="/root/assignment1-basics/pyproject.toml",
+    #   copy=True,
+    # )
+    # .add_local_file(
+    #   local_path="../assignment1-basics/README.md",
+    #   remote_path="/root/assignment1-basics/README.md",
+    #   copy=True,
+    # )
+    # .add_local_file(
+    #   local_path="../assignment1-basics/uv.lock",
+    #   remote_path="/root/assignment1-basics/uv.lock",
+    #   copy=True,
+    # )
+    # .run_commands(
+    #   "pip install -e /root/assignment1-basics",
+    # )
+    # -------------------------------------------------------------------
+
     # This installs only the dependencies list, ignoring the rest, i.e. it does
     # not know that cs336-basics should be installed from a local path.
     .pip_install_from_pyproject("pyproject.toml")
